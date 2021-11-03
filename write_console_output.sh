4d78a77e89e46fde9265b4d1eed2858d29e8ec48
beatricedaniel <beatrice.yk.daniel@protonmail.com>
2021-11-03 13:18:16 +0000

    path changed to use crontab

--- write_console_output.sh
+++ write_console_output.sh
@@ -1,1 +1,1 @@
-cd .. && source env_python3.8/bin/activate && cd write_console_output && python write_console_output.py >> output_$(date +'%Y-%m-%d').log
+source env_python3.8/bin/activate && cd write_console_output && python write_console_output.py >> output_$(date +'%Y-%m-%d').log
